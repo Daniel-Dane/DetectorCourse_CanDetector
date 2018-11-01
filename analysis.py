01f91198e96c45375572cd6163046e867f430940
dnielsen <daniel.stefaniak.nielsen@cern.ch>
2018-11-01 10:40:30 +0100

    fixed names/directories

---
+++ analysis.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  31 08:19:00 2018
+
+@author: dnielsen, fthiele, rignazzi, etty
+"""
+
+import numpy as np
+import sys
+#import matplotlib.pyplot as plt
+#from matplotlib.colors import LogNorm
+
+#from os import path
+#sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+np.random.seed(42)
+
+# milimeters
+std_caliper = 0.01 #?
+std_micrometer = 0.01

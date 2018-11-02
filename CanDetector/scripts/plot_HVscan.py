beb901de760ee980116893c4d7f71b415afcc207
Fabian Thiele <fabian.thiele@posteo.de>
2018-11-02 13:23:36 +0100

    Change ROI of datafiles, working gaus fits for plot_HVscan.py

--- CanDetector/scripts/plot_HVscan.py
+++ CanDetector/scripts/plot_HVscan.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 import logging
 import argparse
@@ -6,6 +6,7 @@ import sys
 import math
 from numpy import genfromtxt
 from rootpy.plotting import Hist
+from ROOT import TF1, kRed
 from matplotlib import gridspec
 from matplotlib.font_manager import FontProperties
 import matplotlib
@@ -52,6 +53,79 @@ def parseArguments(argv=None):
     return args
 
 
+def plotSpec(name, h, r_min=0, r_max=0):
+    pp = PdfPages(name)
+    font0 = FontProperties()
+    font = font0.copy()
+    font.set_style('italic')
+    font.set_weight('bold')
+    font.set_size('x-large')
+    font_wip = font0.copy()
+    font_wip.set_style('italic')
+    font_wip.set_weight('medium')
+
+    plt.figure(figsize=(15, 8), dpi=300)
+    plt.xlabel('xtick', fontsize=5)
+    plt.ylabel('ytick', fontsize=5)
+    gs = gridspec.GridSpec(1, 1)
+
+    gs1 = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec=gs[0],
+                                           width_ratios=[1],
+                                           height_ratios=[1],
+                                           wspace=0.1,
+                                           hspace=0.2)
+
+    ax1 = plt.subplot(gs1[:, :])
+    axes = [ax1]
+
+    for ax in axes:
+
+        # name axes and set maximum of y-axis to accomodate
+        # all histograms inside histstack
+        ax.set_ylabel("Counts")
+        ax.set_xlabel("Channel]")
+        ax.set_title(name.replace(".pdf", ""))
+
+        #ax.errorbar(d_volt, d_y, d_y_unc, d_volt_unc, linestyle='None', color='red')
+        f = TF1("tf_{:}".format(name), "gaus", float(r_min), float(r_max))
+        h.Fit("tf_{:}".format(name), "LLR") # , "R")
+        rplt.hist(h, stacked=False, fill=False, axes=ax)
+
+        print("constant = {:}".format(f.GetParameter(0)))
+        print("mean = {:}".format(f.GetParameter(1)))
+        print("std = {:}".format(f.GetParameter(2)))
+        fwhm = f.GetParameter(2)*2.*math.sqrt(2*math.log(2))
+        print("FWHM = {:}".format(fwhm))
+        print("eval for r_min = {:}: {:}".format(r_min, f.Eval(float(r_min))))
+        xx = range(int(r_min), int(r_max))
+        yy = [f.Eval(x) for x in xx]
+        plt.plot(xx, yy, 'r')
+        ax.axvline(x=r_min)
+        ax.axvline(x=r_max)
+
+
+        # make ATLAS fonts in plot
+        ax.text(0.2, 0.9, '1-Group',
+                verticalalignment='bottom', horizontalalignment='left',
+                fontproperties=font, transform=ax.transAxes)
+        ax.text(0.2, 0.87, 'Data',
+                verticalalignment='bottom', horizontalalignment='left',
+                fontproperties=font_wip, transform=ax.transAxes)
+        ax.text(0.8, 0.9, "Mean = {:.2f}".format(f.GetParameter(1)),
+                verticalalignment='bottom', horizontalalignment='left',
+                fontproperties=font_wip, transform=ax.transAxes)
+        ax.text(0.8, 0.87, "FWHM = {:.2f}".format(fwhm),
+                verticalalignment='bottom', horizontalalignment='left',
+                fontproperties=font_wip, transform=ax.transAxes)
+
+        ax.legend(fontsize='small')
+        ax.grid()
+
+    plt.show()
+    plt.savefig(pp, format='pdf')
+    pp.close()
+
+
 def mca_to_hist(filename):
     roi_on = False
     data_on = False
@@ -70,7 +144,10 @@ def mca_to_hist(filename):
             roi_on = False
 
         if data_on:
-            h.SetBinContent(data_line, line.split()[0])
+            if line.find("END") > -1:
+                data_on = False
+                continue
+            h.SetBinContent(data_line, float(line.split()[0]))
             data_line += 1
 
         if line.find("ROI") > -1:
@@ -81,7 +158,7 @@ def mca_to_hist(filename):
 
     print("region of interest: {:} to {:}".format(r_min, r_max))
 
-    return h
+    return (h, r_min, r_max)
 
 
 def main(argv):
@@ -99,7 +176,10 @@ def main(argv):
 
     am_confs = ["100_1136", "100_1191", "100_1244", "100_1297", "100_1351", "100_1399", "10_1665", "10_1712", "10_1758", "20_1559", "20_1603", "20_1666", "2_1900", "2_1951", "2_2001", "40_1397", "40_1455", "40_1502", "40_1559", "4_1757", "4_1808", "4_1858", "4_1899"]
 
-    print(mca_to_hist("../data/mca/am_"+am_confs[5]+".mca"))
+    for v in am_confs:
+        name = "../data/mca/am_"+v+".mca"
+        (h, r_min, r_max) = mca_to_hist(name)
+        plotSpec(name.replace(".mca", ".pdf"), h, r_min, r_max)
 
 
     d_y = []
@@ -112,54 +192,6 @@ def main(argv):
         d_y_unc.append(math.sqrt(math.pow(rel_unc_mean, 2)+
                                  math.pow(rel_unc_fwhm, 2))*d_y[idx])
 
-    pp = PdfPages(args.file.replace(".txt", "")+'.pdf')
-    font0 = FontProperties()
-    font = font0.copy()
-    font.set_style('italic')
-    font.set_weight('bold')
-    font.set_size('x-large')
-    font_wip = font0.copy()
-    font_wip.set_style('italic')
-    font_wip.set_weight('medium')
-
-    plt.figure(figsize=(10, 8), dpi=300)
-    plt.xlabel('xtick', fontsize=5)
-    plt.title("test")
-    plt.ylabel('ytick', fontsize=5)
-    gs = gridspec.GridSpec(1, 1)
-
-    gs1 = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec=gs[0],
-                                           width_ratios=[1],
-                                           height_ratios=[1],
-                                           wspace=0.1,
-                                           hspace=0.2)
-
-    ax1 = plt.subplot(gs1[:, :])
-    axes = [ax1]
-
-    for ax in axes:
-
-        # name axes and set maximum of y-axis to accomodate
-        # all histograms inside histstack
-        ax.set_ylabel("FWHM/Mean")
-        ax.set_xlabel("Voltage [V]")
-
-        ax.errorbar(d_volt, d_y, d_y_unc, d_volt_unc, linestyle='None', color='red')
-
-        # make ATLAS fonts in plot
-        ax.text(0.2, 0.9, '1-Group',
-                verticalalignment='bottom', horizontalalignment='left',
-                fontproperties=font, transform=ax.transAxes)
-        ax.text(0.2, 0.87, 'Data',
-                verticalalignment='bottom', horizontalalignment='left',
-                fontproperties=font_wip, transform=ax.transAxes)
-
-        ax.legend(fontsize='small')
-        ax.grid()
-
-    plt.show()
-    plt.savefig(pp, format='pdf')
-    pp.close()
 
 
 

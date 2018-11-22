8863369694c2650b5e85874c375c30976b726f8f
bourdeet <27770178+bourdeet@users.noreply.github.com>
2018-11-22 15:03:52 +0100

    added graphics in semiconductor report + fixed a bug in the ln(M) plots

--- CanDetector/analysis/compute_uncertainties.py
+++ CanDetector/analysis/compute_uncertainties.py
@@ -25,7 +25,11 @@ class gas_properties:
         self.pressure = 1.0 # Units = fraction of standard pressure (p/[1 bar])
         self.pressure_unc = 0.001 # Based on monitoring over a day
         self.temperature = 1.0# Units = fraction of standard temperature (T/[273.15 K])
-
+        self.K = 4.8e4   #Units of V/cm/atm
+        self.K_unc = 0.3e4   #Units of V/cm/atm
+        self.dU = 23.6   #Units of eV
+        self.dU_unc = 5.4   #Units of eV
+        
     def __getitem__(self,key):
         return self.__dict__[key]
     
@@ -66,16 +70,24 @@ class cangeo:
 
     def __init__(self):
         #initialize with the values in
-        # our lab report (can values)
+        # theoretical_calc.py
+        # this file has measurements in mm
+        sys.path.append("../scripts/")
+        from theoretical_calc import cider_diameter,std_caliper
+        from theoretical_calc import cider_wall,std_micrometerscrew
+        from theoretical_calc import anodewire_diameter, std_micrometerscrew
+        
         #outer can diameter)
-        self.out_d = 8.     #units: cm
-        self.out_d_unc = 0.001
+        self.out_d = np.mean(cider_diameter)/10.  #units: cm
+        #self.out_d_unc = std_caliper/10. #units: cm
+        
         #thickness
-        self.thick = 0.0105 # units cm
-        self.thick_unc = 0.001
+        self.thick = cider_wall/10.              # units cm
+        #self.thick_unc = std_micrometerscrew/10. #units: cm
+        
         #Wire diameter      #units cm
-        self.wire = 0.005
-        self.wire_unc = 0.001
+        self.wire = anodewire_diameter/10.  #
+        #self.wire_unc = std_micrometerscrew/10.
 
     def __getitem__(self,key):
         return self.__dict__[key]
@@ -111,10 +123,11 @@ class cangeo:
     # prior to calling this function
     
     def compute_useful_geo(self):
+
         #anode radius
-        self.ra = self.wire/2.  #units cm
+        self.ra = self['wire']/2.  #units cm
         #cathode radius
-        self.rc = self.out_d/.2-self.thick  #units cm             
+        self.rc = self['out_d']/2.-self['thick']  #units cm             
     
         
         
@@ -136,21 +149,59 @@ def lnm(gas,geo,V):
     Emin = gas['Emin']
     dV = gas['dv']
     
-    return np.log(V)/np.log(b/a)*np.log(2)/dV*(np.log(V/(a*np.log(b/a)))-np.log(Emin*rho_ratio))
+    return V/np.log(b/a)*np.log(2)/dV*(np.log(V/(a*np.log(b/a)))-np.log(Emin*rho_ratio))
+
+
+def lnm_beer(gas,geo,V):
+    '''
+    Compute the berrcan version of the lnm formula
+    
+    inputs are:
+    gas: gas_property object (see definition above)
+    geo: detector_geometry object (see above)
+    V: operating voltage (in kV)
+    '''
+    a = geo['ra']
+    b = geo['rc']
+    p = gas['pressure']
+    K = gas['K']
+    dU = gas['dU']
+
+    V = 1000.*V # voltage is in volt in this formula
+
+    A =  V/np.log(b/a)
+
+    B = np.log(2.)/dU
+
+    C = np.log(V/(p*a*np.log(b/a)))-np.log(K)
+
+    return A*B*C
 
 
 if __name__=='__main__':
 
-    ntrials = 1000000
+    import argparse
+
+    parser = argparse.ArgumentParser("generate MC data of Gain")
+    parser.add_argument('--ntrials',
+                        type=int,
+                        help='# of trials to run per voltage value',
+                        default=1000)
+    parser.add_argument('--output',
+                        help='output pickle file that stores the sigma contours.',
+                        default='uncertainties_lnm.p')
+    args = parser.parse_args()
+
+    
 
     lnm_distributions = {}
-    voltages = np.linspace(1.3,4.,101)
+    voltages = np.linspace(0.8,4.,101)
     
     for OV in voltages: #scan accross operating voltages
 
         lnm_distributions[OV] = []
    
-        for i in range(0,ntrials):
+        for i in range(0,args.ntrials):
 
             gas = gas_properties()
             gas.wigle()
@@ -191,7 +242,9 @@ if __name__=='__main__':
                  one_pc,
                  ninety,
                  ninety9,
-                 median],open("uncertainties_lnm_sub_p0.001_p_uncertainty.p","wb"))
+                 median,
+                 voltages],open(args.output,"wb"))
+    print "saved output to ",args.output
 
     voltages = X
     plt.fill_between(voltages,one_pc,ninety9,label=r'$2\sigma$',color='m')
@@ -205,7 +258,7 @@ if __name__=='__main__':
     plt.xlabel('Operating voltage (kV)')
     plt.ylabel('M')
     plt.legend()
-    plt.ylim([1,6.])
+    plt.ylim([0,60.])
     plt.show()
 
             
--- CanDetector/analysis/plot_m_factor.py
+++ CanDetector/analysis/plot_m_factor.py
@@ -7,6 +7,11 @@ import scipy.optimize as optimization
 def func(x, a, b):
     return a*x + b 
 
+#****************************************************************************
+# Preamplifier business
+
+# A. Extract the dimensionless preamp gain
+
 # gives the MCA-to-preamp voltage relationship
 calib = np.genfromtxt("../data/cidercan_calibration.txt",skip_header=1)
 input_v = calib[:,0]/1000.
@@ -24,12 +29,30 @@ Fit,covariance  = optimization.curve_fit(func,input_v ,outputv, sigma=Dy_preamp,
 
 a = Fit[0]
 b = Fit[1]
-print covariance
 perr = np.sqrt(np.diag(covariance)) # 1-sigma error on the parameter
-print perr[0]
-
 plt.plot(input_v,func(input_v,a,b),label='linear fit')
-plt.text(0.08,4.,r'Preamplifier gain: %.02f $\pm$ %.02f'%(a,perr[0]))
+plt.text(0.08,4.,r'Preamplifier gain: %.02f $\pm$ %.02f'%(a/10.,perr[0]/10.)) # divide by 10 to remove effect of the coarse gain setting
+plt.show()
+
+
+# B. Extract gain, but including the MCA-to-volt conversion factor
+output_mca = calib[:,2]
+Dy_mca = calib[:,4]
+
+plt.errorbar(input_v,output_mca,yerr=Dy_mca,fmt='s',color='r')
+plt.title('Preamplifier gain')
+plt.xlabel('input voltage (V)',fontsize=13)
+plt.ylabel('Digitized Output (MCA)',fontsize=13) # As measured with the MCA
+
+# Fit a linear function. Print out the slope of the preamplifier gain and its uncertainty
+Fit_mca,covariance_mca  = optimization.curve_fit(func,input_v ,output_mca, sigma=Dy_mca,
+                                         absolute_sigma=True)
+
+mca_preampgain = Fit_mca[0]
+mca_b = Fit_mca[1]
+mca_perr = np.sqrt(np.diag(covariance_mca)) # 1-sigma error on the parameter
+plt.plot(input_v,func(input_v,mca_preampgain,mca_b),label='linear fit',color='k')
+plt.text(0.06,200.,r'Preamplifier gain: %.02f $\pm$ %.02f MCA\V'%(mca_preampgain/10.,mca_perr[0]/10.))
 plt.show()
 
 
@@ -37,12 +60,11 @@ plt.show()
 Data_fe = np.genfromtxt("../data/cidercan_fe_HVscan.txt",skip_header=1)
 Data_am = np.genfromtxt("../data/cidercan_am_HVscan.txt",skip_header=1)
 
-# columns are the following for the HV scans
-#[Coarse gain]	[Voltage kV]	[MCA mean]	[MCA FWHM]	[FWHM/mean]
 
 
-# Step 1: Estimate the uncertainty on the coarse gain amplifier
-#
+
+#*************************************************************************************
+# Coarse gain business
 # For this we have 4 points where the same voltage is probed in two different setting
 
 point_1 = Data_fe[5:7,:]
@@ -104,29 +126,141 @@ plt.errorbar(x_values+0.4,coarse_gain,yerr=coarse_gain_unc,fmt='o',color='r',lab
 ax = plt.gca()
 ax.set_xticks(x_values+0.1)
 ax.set_xticklabels(x_fe)
-plt.title("Coarse Gain uncertainty")
-plt.xlabel('Coarse gain step',fontsize=13)
+plt.title("Coarse Gain Ratios")
+plt.xlabel('Ratio between steps',fontsize=13)
 plt.ylabel('Measured Gain',fontsize=13)
 plt.legend()
 plt.show()
 
+
+
 #******************************************************************
 
 # Part where you plot the theoretical joker bands
+import pickle
+import seaborn as sns
 
-voltages = X
-plt.fill_between(voltages,one_pc,ninety9,label=r'$2\sigma$',color='m')
-plt.fill_between(voltages,ten_pc,ninety,label=r'$1\sigma$',color='g')
-plt.plot(voltages,one_pc,'m')
-plt.plot(voltages,ninety9,'m')
-plt.plot(voltages,ten_pc,'g')
-plt.plot(voltages,ninety,'g')
+dataname = "./test.p"
+data = pickle.load(open(dataname))
+
+ten_pc = data[0]
+one_pc = data[1]
+ninety = data[2]
+ninety9= data[3]
+median = data[4]
+voltages=data[5]
+
+plt.fill_between(voltages,one_pc,ninety9,label=r'$2\sigma$',color='#deebf7')
+plt.fill_between(voltages,ten_pc,ninety,label=r'$1\sigma$',color='#9ecae1')
+plt.plot(voltages,one_pc,'#deebf7')
+plt.plot(voltages,ninety9,'#deebf7')
+plt.plot(voltages,ten_pc,'#9ecae1')
+plt.plot(voltages,ninety,'#9ecae1')
 plt.plot(voltages,median,'k',label='median')
 
+#**********************************************************************
+# Part where you plot the data, divided by the proper factor
+M_coarse_Fe = Data_fe[:,0] # coarse gain setting
+M_X_Fe = Data_fe[:,1]      # operating voltage
+M_MCA_Fe = Data_fe[:,2]    # Signal in MCA counts
+M_MCA_Fe_unc = Data_fe[:,3]
+
+# Part where you plot the data, divided by the proper factor
+M_coarse_am = Data_am[:,0] # coarse gain setting
+M_X_am = Data_am[:,1]/1000.      # operating voltage
+M_MCA_am = Data_am[:,2]    # Signal in MCA counts
+M_MCA_am_unc = Data_am[:,3]
+
+
+
+preamp_gain = mca_preampgain/10. # preamp data was taken at a coarse gain of 10. Removing it form the equation
+preamp_unc  = mca_perr[0]/10.
+
+# Write out the ratios explicitely:
+G100_G40=coarse_gain[0]
+G40_G20=coarse_gain[1]
+G20_G10=coarse_gain[2]
+G10_G4=coarse_gain[3]
+G4_G2=coarse_gain[4]
+
+G100_G40_e=coarse_gain_unc[0]
+G40_G20_e =coarse_gain_unc[1]
+G20_G10_e =coarse_gain_unc[2]
+G10_G4_e  =coarse_gain_unc[3]
+G4_G2_e   =coarse_gain_unc[4]
+
+#Define the correct coarse gain factor to use
+M_Gratio  = np.zeros(len(M_coarse_Fe))
+M_Gratio_unc = np.zeros(len(M_coarse_Fe))
+
+for g,i in zip(M_coarse_Fe,range(0,len(M_coarse_Fe))):
+
+    if g==2:
+        M_Gratio[i] = 10./G10_G4/G4_G2
+        M_Gratio_unc[i] = M_Gratio[i]*np.sqrt((G10_G4_e/G10_G4)**2.+(G4_G2_e/G4_G2)**2.)
+    elif g==4:
+        M_Gratio[i] = 10./G10_G4
+        M_Gratio_unc[i] = M_Gratio[i]*(G10_G4_e/G10_G4)
+    elif g==10:
+        M_Gratio[i] = 10.
+        M_Gratio_unc[i] = 0.0
+    elif g==20:
+        M_Gratio[i] = 10.*G20_G10
+        M_Gratio_unc[i] = M_Gratio[i]*(G20_G10_e/G20_G10)
+    elif g==40:
+        M_Gratio[i] = 10.*G20_G10*G40_G20
+        M_Gratio_unc[i] = M_Gratio[i]*np.sqrt((G20_G10_e/G20_G10)**2.+(G40_G20_e/G40_G20)**2.)
+    elif g==100:
+        M_Gratio[i] = 10.*G20_G10*G40_G20*G100_G40
+        M_Gratio_unc[i] = M_Gratio[i]*np.sqrt((G20_G10_e/G20_G10)**2.+(G40_G20_e/G40_G20)**2.+(G100_G40_e/G100_G40)**2.)
+        
+Q_detector_fe = M_MCA_Fe/preamp_gain*(1e-12)/1.602e-19/M_Gratio
+Q_detector_fe_unc = Q_detector_fe*np.sqrt((preamp_unc/preamp_gain)**2.+(M_Gratio_unc/M_Gratio)**2.+(M_MCA_Fe_unc/M_MCA_Fe)**2.)
+M_Fe = np.log(Q_detector_fe/227.)
+M_Fe_unc = np.log(Q_detector_fe_unc/227.)/M_Fe
+
+
+#***************************************************************************
+# Doing the Americium stuff
+M_Gratio_am  = np.zeros(len(M_coarse_am))
+M_Gratio_am_unc = np.zeros(len(M_coarse_am))
+         
+for g,i in zip(M_coarse_am,range(0,len(M_coarse_am))):
+
+    if g==2:
+        M_Gratio_am[i] = 10./G10_G4/G4_G2
+        M_Gratio_am_unc[i] = M_Gratio_am[i]*np.sqrt((G10_G4_e/G10_G4)**2.+(G4_G2_e/G4_G2)**2.)
+    elif g==4:
+        M_Gratio_am[i] = 10./G10_G4
+        M_Gratio_am_unc[i] = M_Gratio_am[i]*(G10_G4_e/G10_G4)
+    elif g==10:
+        M_Gratio_am[i] = 10.
+        M_Gratio_am_unc[i] = 0.0
+    elif g==20:
+        M_Gratio_am[i] = 10.*G20_G10
+        M_Gratio_am_unc[i] = M_Gratio_am[i]*(G20_G10_e/G20_G10)
+    elif g==40:
+        M_Gratio_am[i] = 10.*G20_G10*G40_G20
+        M_Gratio_am_unc[i] = M_Gratio_am[i]*np.sqrt((G20_G10_e/G20_G10)**2.+(G40_G20_e/G40_G20)**2.)
+    elif g==100:
+        M_Gratio_am[i] = 10.*G20_G10*G40_G20*G100_G40
+        M_Gratio_am_unc[i] = M_Gratio_am[i]*np.sqrt((G20_G10_e/G20_G10)**2.+(G40_G20_e/G40_G20)**2.+(G100_G40_e/G100_G40)**2.)
+             
+
+Q_detector_am = M_MCA_am/preamp_gain*(1e-12)/1.602e-19/M_Gratio_am
+Q_detector_am_unc = Q_detector_am*np.sqrt((preamp_unc/preamp_gain)**2.+(M_Gratio_am_unc/M_Gratio_am)**2.+(M_MCA_am_unc/M_MCA_am)**2.)
+
+M_am = np.log(Q_detector_am/(2290.))
+M_am_unc = Q_detector_am_unc/(2290.)/M_am
+
+plt.errorbar(M_X_am,M_am,yerr=M_am_unc,fmt='sr',label=r'$Am^{241}$',color='#de2d26')
+plt.errorbar(M_X_Fe,M_Fe,yerr=M_Fe_unc,fmt='o',label=r'$Fe^{55}$',color='#31a354')
+
 plt.xlabel('Operating voltage (kV)')
-plt.ylabel('M')
+plt.ylabel(r'$\ln(M)$')
 plt.legend()
-plt.ylim([1,6.])
+plt.xlim([0,4])
+plt.ylim([-2.,15.])
 plt.show()
 
             
